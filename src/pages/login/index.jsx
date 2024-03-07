b49691c4aefc41581cb9741277468d1d20b97fdd
Pazil24 <azilafupriscilla@gmail.com>
2024-03-07 13:57:44 +0000

    Login base created

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,4 +1,13 @@
 
 export default function Login() {
-    return <p>Login</p>
+    return (
+        <div>
+            <h2>Sign Up to Stream</h2>
+            <form>
+                <input type="text" placeholder="Enter email" />
+                <input type="password" placeholder="Enter password" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    )
 }
