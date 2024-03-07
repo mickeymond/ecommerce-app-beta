83b33228714b7797d190bad40518b74538a8dee6
DianaDzakah <dianadzakah1@gmail.com>
2024-03-07 17:23:50 +0000

    changes done to hero

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,13 @@
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
-
 export default function Home() {
     return (
         <>
             <Navbar />
-            <>
-            
-            </>
+            <div class="bg-[url('https://images.unsplash.com/photo-1573382121730-2005f88463a2')] bg-cover bg-center h-screen">
+            <h2>Order and Receive with Ease</h2>
+            <butt>Explore</butt>
+            </div>
             <Footer />
         </>
     );
