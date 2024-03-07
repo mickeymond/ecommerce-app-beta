19c06a9ce45c79ab945492a58d00cfb3623e97e2
ednaakpene <edvalerie18@gmail.com>
2024-03-07 18:04:29 +0000

    cart page

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,6 +1,6 @@
 import Footer from "../../components/footer";
 import Navbar from "../../components/navbar";
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export default function Cart() {
   const [cartItems, setCartItems] = useState([
@@ -11,6 +11,7 @@ export default function Cart() {
       image:
         "https://images.unsplash.com/photo-1623227866882-c005c26dfe41?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       alt: "strawberry",
+      price: 20,
     },
     {
       id: 2,
@@ -19,6 +20,7 @@ export default function Cart() {
       image:
         "https://images.unsplash.com/photo-1528825871115-3581a5387919?q=80&w=1430&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       alt: "banana",
+      price: 30,
     },
     {
       id: 3,
@@ -27,11 +29,51 @@ export default function Cart() {
       image:
         "https://images.unsplash.com/photo-1573067485648-4cb2618b5e09?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       alt: "bell peppers",
+      price: 50,
     },
   ]);
 
+  const calculateSubtotal = useCallback(() => {
+    return cartItems.reduce(
+      (total, item) => total + item.quantity * item.price,
+      0
+    );
+  }, [cartItems]);
+
+  useEffect(() => {
+    // Update subtotal whenever cartItems change
+    const subtotal = calculateSubtotal();
+    // update cart
+    console.log("Subtotal:", subtotal);
+  }, [cartItems, calculateSubtotal]);
+
   const handleRemoveItem = (itemId) => {
     const updatedCart = cartItems.filter((item) => item.id !== itemId);
+    setCartItems(updatedCart);
+  };
+
+  const handleQuantityChange = (itemId, newQuantity) => {
+    const updatedCart = cartItems.map((item) =>
+      item.id === itemId ? { ...item, quantity: newQuantity } : item
+    );
+    setCartItems(updatedCart);
+  };
+
+  const increaseQuantity = (itemId) => {
+    const updatedCart = cartItems.map((item) =>
+      item.id === itemId
+        ? { ...item, quantity: parseInt(item.quantity, 10) + 1 }
+        : item
+    );
+    setCartItems(updatedCart);
+  };
+
+  const decreaseQuantity = (itemId) => {
+    const updatedCart = cartItems.map((item) =>
+      item.id === itemId && item.quantity > 1
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
     setCartItems(updatedCart);
   };
 
@@ -60,22 +102,59 @@ export default function Cart() {
 
                     <div>
                       <h2 class="text-sm text-gray-900">{item.name}</h2>
+
+                      <dl class="mt-0.5 space-y-px text-[10px] text-gray-600">
+                        <div>
+                          <dt class="inline">Price:</dt>
+                          <dd class="inline">{item.price}</dd>
+                        </div>
+                      </dl>
                     </div>
 
                     <div class="flex flex-1 items-center justify-end gap-2">
                       <form>
-                        <label for="Line1Qty" class="sr-only">
-                          {" "}
-                          Quantity{" "}
+                        <label
+                          htmlFor={`Line${item.id}Qty`}
+                          className="sr-only"
+                        >
+                          Quantity
                         </label>
+                        <div class="flex items-center gap-1">
+                          <button
+                            onClick={() => decreaseQuantity(item.id)}
+                            type="button"
+                            class="size-10 leading-10 text-gray-600 transition hover:opacity-75"
+                          >
+                            &minus;
+                          </button>
 
-                        <input
-                          type="number"
-                          min="1"
-                          value="1"
-                          id="Line1Qty"
-                          class="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-                        />
+                          <input
+                            type="number"
+                            min="1"
+                            value={item.quantity}
+                            id={`Line${item.id}Qty`}
+                            className="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+                            onChange={() => {}}
+                            onBlur={() =>
+                              handleQuantityChange(item.id, item.quantity)
+                            }
+                            onKeyDown={(e) => {
+                              if (e.key === "Enter") {
+                                e.preventDefault();
+                                e.target.blur();
+                                handleQuantityChange(item.id, item.quantity);
+                              }
+                            }}
+                            readOnly
+                          />
+                          <button
+                            onClick={() => increaseQuantity(item.id)}
+                            type="button"
+                            class="size-10 leading-10 text-gray-600 transition hover:opacity-75"
+                          >
+                            +
+                          </button>
+                        </div>
                       </form>
 
                       <button
@@ -102,102 +181,6 @@ export default function Cart() {
                     </div>
                   </li>
                 ))}
-
-                {/* <li class="flex items-center gap-4">
-                  <img
-                    src="https://images.unsplash.com/photo-1528825871115-3581a5387919?q=80&w=1430&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="banana"
-                    class="size-16 rounded object-cover"
-                  />
-
-                  <div>
-                    <h2 class="text-sm text-gray-900">Banana</h2>
-                  </div>
-
-                  <div class="flex flex-1 items-center justify-end gap-2">
-                    <form>
-                      <label for="Line2Qty" class="sr-only">
-                        {" "}
-                        Quantity{" "}
-                      </label>
-
-                      <input
-                        type="number"
-                        min="1"
-                        value="1"
-                        id="Line2Qty"
-                        class="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-                      />
-                    </form>
-
-                    <button class="text-gray-600 transition hover:text-red-600">
-                      <span class="sr-only">Remove item</span>
-
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="h-4 w-4"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </li>
-
-                <li class="flex items-center gap-4">
-                  <img
-                    src="https://images.unsplash.com/photo-1573067485648-4cb2618b5e09?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="bell peppers"
-                    class="size-16 rounded object-cover"
-                  />
-
-                  <div>
-                    <h2 class="text-sm text-gray-900">Bell pepper</h2>
-                  </div>
-
-                  <div class="flex flex-1 items-center justify-end gap-2">
-                    <form>
-                      <label for="Line3Qty" class="sr-only">
-                        {" "}
-                        Quantity{" "}
-                      </label>
-
-                      <input
-                        type="number"
-                        min="1"
-                        value="1"
-                        id="Line3Qty"
-                        class="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
-                      />
-                    </form>
-
-                    <button class="text-gray-600 transition hover:text-red-600">
-                      <span class="sr-only">Remove item</span>
-
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="h-4 w-4"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </li> */}
               </ul>
 
               <div class="mt-8 flex justify-end border-t border-gray-100 pt-8">
@@ -205,55 +188,23 @@ export default function Cart() {
                   <dl class="space-y-0.5 text-sm text-gray-700">
                     <div class="flex justify-between">
                       <dt>Subtotal</dt>
-                      <dd>£250</dd>
-                    </div>
-
-                    <div class="flex justify-between">
-                      <dt>VAT</dt>
-                      <dd>£25</dd>
-                    </div>
-
-                    <div class="flex justify-between">
-                      <dt>Discount</dt>
-                      <dd>-£20</dd>
+                      <dd>${calculateSubtotal()}</dd>
                     </div>
 
                     <div class="flex justify-between !text-base font-medium">
                       <dt>Total</dt>
-                      <dd>£200</dd>
+                      <dd>${calculateSubtotal()}</dd>{" "}
                     </div>
                   </dl>
 
                   <div class="flex justify-end">
-                    <span class="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="-ms-1 me-1.5 h-4 w-4"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
-                        />
-                      </svg>
-
-                      <p class="whitespace-nowrap text-xs">
-                        2 Discounts Applied
-                      </p>
-                    </span>
+                    <span class="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700"></span>
                   </div>
 
                   <div class="flex justify-end">
-                    <a
-                      href="#"
-                      class="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
-                    >
+                    <button class="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600">
                       Checkout
-                    </a>
+                    </button>
                   </div>
                 </div>
               </div>
