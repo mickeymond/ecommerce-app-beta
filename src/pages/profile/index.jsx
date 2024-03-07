e9f98c3f9c5b84485acaba5a335fcf100a95f2ce
dezmymachine <desmondkwakucoffie195@gmail.com>
2024-03-07 14:40:45 +0000

    initial creation of profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -2,11 +2,11 @@ import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 
 export default function Profile() {
-    return (
-        <>
-            <Navbar />
-            <p>User profile goes here</p>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <p>User profile goes here</p>
+      <Footer />
+    </>
+  );
 }
