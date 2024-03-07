a72da9acf7d9e238697037fec5a44f88bdc11371
mrkabby <antwikenneth14@gmail.com.com>
2024-03-07 09:55:31 +0000

    added the navbar

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  );
-}
+    return (
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    );
+  }
