634b2374260db88ac0a765845a5da6c529103dea
theophilusnuer <tphilus003@gmail.com>
2024-03-08 17:38:21 +0000

    displaying the items in grid style

--- src/components/catalogueList/index.jsx
+++ src/components/catalogueList/index.jsx
@@ -2,8 +2,8 @@
 export default function CatalogueList({ title, images, description, price }) {
     return (
         <section className="md:container md:mx-auto">
-            <div className="mt-8 grid grid-cols-4 gap-10">
-                <div className="card rounded overflow-hidden shadow-md">
+            <div>
+                <div className="h-90 w-64 card rounded overflow-hidden shadow-md">
                     <div>
                         <img src={images} alt={title} className="w-full sm:h-48 object-cover" />
                     </div>
--- src/pages/catalogue/index.jsx
+++ src/pages/catalogue/index.jsx
@@ -23,11 +23,14 @@ export default function Catalogue() {
 useEffect(getProducts, []) 
 
     return (
-        <>
+        <div>
             <Navbar />
+            
+            <div className="p-10 grid grid-cols-4 gap-5"> 
          {products.map((product) => <CatalogueList key={product.id} id={product.id} title={product.title} images={product.images} description={product.description} price={product.price}  />)
          }
+         </div>
             <Footer />
-        </>
+        </div>
     );
 }
